b670cd9dc012d053a631399dabd14885e0cfc7db
opr-clau <masterclau2005@gmail.com>
2025-10-20 18:11:30 +0300

    Tema1

---
+++ Tema1.py
@@ -0,0 +1,6 @@
+articol = "Compania Municipală Termoenergetica Bucureşti anunţă, luni, că în următoarele zile se vor face lucrări de remediere a unor avarii pe conductele reţelei termice, în mai multe zone din Capitală. Potrivit sursei citate, intervenţiile au durate diferite de realizare, în funcţie de complexitatea fiecărei lucrări. Sunt afectaţi locuitorii din aproximativ 250 de blocuri."
+articol_x = articol[(len(articol)//2):]
+articol_y = articol[:(len(articol)//2)]
+articol_y_modificat = articol_y.upper().replace(" ","")
+articol_x_modificat = articol_x.replace(",","").replace(".","").replace("?","").replace("!","").capitalize()[::-1]
+print(articol_y_modificat + articol_x_modificat)
